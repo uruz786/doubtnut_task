b04b86bacba8f7a9f630d446427a5c44aa0f74e1
Uruz <uruzfatima767@gmail.com>
2021-02-05 03:43:06 +0530

    new-commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Exercise from './Exercise.jsx';
+
+
 ReactDOM.render(< Exercise />,
   document.getElementById('root')
 );
